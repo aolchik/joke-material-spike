e71f846ad8d6dd80cfb786813cc6429945ff4aa1
Alejandro Olchik <aolchik@ionatec.com.br>
2017-06-21 06:40:44 -0300

    Descricao dos testes em portugues.

--- src/app/joke/joke.component.spec.ts
+++ src/app/joke/joke.component.spec.ts
@@ -34,27 +34,35 @@ describe('JokeComponent', () => {
     compiled = fixture.debugElement.nativeElement;
   });
 
-  it('is created', () => {
-    expect(component).toBeTruthy();
+  describe('construtor', () => {
+    it('cria objeto', () => {
+      expect(component).toBeTruthy();
+    });
+
+    it('mostra piada', () => {
+      expect(compiled.querySelector('h2').textContent).toBe("piada");
+    });
+
+    it('mostra punchline', () => {
+      expect(compiled.querySelector('.punchline').textContent).toBe("remate");
+    });
   });
 
-  it('shows joke', () => {
-    expect(compiled.querySelector('h2').textContent).toBe("joke");
-  });
-
-  it('hides punchline', () => {
-    let pe = compiled.querySelector('.punchline');
-    expect(compiled.querySelector('.punchline')).not.toBe(null);
-    expect(compiled.querySelector('.punchline').getAttribute('hidden')).not.toBe(null);
+  describe('botão diga-me', () => {
+    it('esconde e mostra remate', () => {
+       pending();
+    });
   });
 });
 
 describe('Joke', () => {
-  it('assigns joke, punchline and hide', () => {
-    let j = new Joke("joke", "punchline", true);
-    expect(j.joke).toBe("joke");
-    expect(j.punchline).toBe("punchline");
-    expect(j.hide).toBe(true);
+  describe('construtor', () => {
+    it('atribui piada, remate and visualização padrão', () => {
+      let j = new Joke("joke", "punchline", true);
+      expect(j.joke).toBe("joke");
+      expect(j.punchline).toBe("punchline");
+      expect(j.hide).toBe(true);
+    });
   });
 });
 
@@ -63,5 +71,5 @@ describe('Joke', () => {
   template: '<joke [joke]="joke"></joke>'
 })
 class TestComponentWrapper {
-  joke = new Joke("joke", "punchline", true)
+  joke = new Joke("piada", "remate", true)
 }
