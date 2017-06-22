9221ce7a237cd09ae41d6c4660b0caf0338c184b
Alejandro Olchik <aolchik@ionatec.com.br>
2017-06-22 06:31:04 -0300

    Adicionar tema padrao do material

--- karma.conf.js
+++ karma.conf.js
@@ -28,6 +28,9 @@ module.exports = function (config) {
     logLevel: config.LOG_INFO,
     autoWatch: true,
     browsers: ['Chrome'],
-    singleRun: false
+    singleRun: false,
+    files: [
+      { pattern: './node_modules/@angular/material/prebuilt-themes/indigo-pink.css' }
+    ]
   });
 };
